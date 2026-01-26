9648e6ae25631fa75317222c3d28bbffa4b128fd
Krish A. Patel <krishpatel2067@gmail.com>
2026-01-26 18:21:39 -0500

    Create functionality: read phase and create/append to respective file instead of a giant master file for better organization

--- src/backend/prod/metadata/phases.js
+++ src/backend/prod/metadata/phases.js
@@ -1,7 +1,9 @@
-const phases = {
-    // phase_name_in_snake_case : { start: "timestamp in GMT-5 (EST)", end: "timestamp in GMT-5 (EST)" }
-    focus_group_1: { start: "2026-01-24T00:00:00-05:00", end: "2026-01-24T23:59:59-05:00" },
-    TEST: { start: "2026-01-25T00:00:00-05:00", end: "2026-01-26T23:59:59-05:00" },
-};
+const phases = [
+    // { name: "phase_name_in_snake_case", start: "timestamp in GMT-5 (EST)", end: "timestamp in GMT-5 (EST)" }
+    { name: "focus_group_1", start: "2026-01-24T00:00:00-05:00", end: "2026-01-24T23:59:59-05:00" },
+    { name: "TEST3", start: "2026-01-26T00:00:00-05:00", end: "2026-01-26T23:59:59-05:00" },
+    { name: "TEST4", start: "2026-01-27T00:00:00-05:00", end: "2026-01-27T23:59:59-05:00" },
+    { name: "TEST5", start: "2026-01-28T00:00:00-05:00", end: "2026-01-28T23:59:59-05:00" },
+];
 
 console.log(JSON.stringify(phases));
--- src/backend/prod/process_response/index.js
+++ src/backend/prod/process_response/index.js
@@ -6,7 +6,6 @@ import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3
 import { validateResponse } from './validateResponse.js';
 // Step 1: S3 Configuration
 const S3_BUCKET = process.env.S3_BUCKET_NAME;
-const S3_KEY = 'survey-responses-master.json';
 const s3 = new S3Client({ region: process.env.AWS_REGION }); // AWS_REGION is pre-defined by AWS
 
 // Step 2: Express.js Setup
@@ -18,21 +17,44 @@ app.post('/survey', async (req, res) => {
     try {
         const { data: newResponse } = req.body;
 
-        // Step 3.1 Fetch the existing master file from S3
-        console.log('Fetching and parsing existing master file from S3...');
-        let masterData = [];
+        console.log('Reading phases file...');
+        let phasesData = [];
+
         try {
-            const response = await s3.send(new GetObjectCommand({ Bucket: S3_BUCKET, Key: S3_KEY }));
+            const phases = await s3.send(new GetObjectCommand({ Bucket: S3_BUCKET, Key: 'metadata/phases.json' }));
+            const phasesBodyContents = await phases.Body.transformToString();
+            phasesData = JSON.parse(phasesBodyContents);
+        } catch (e) {
+            console.error('Error fetching current phase:', e);
+            return res.status(500).json({ message: 'Error accessing phases file' });
+        }
+
+        const currDate = new Date();
+        let currPhase = currDate.getMonth() + 1 + "-" + currDate.getFullYear();
+        for (const phase of phasesData) {
+            if (currDate >= new Date(phase.start) && currDate <= new Date(phase.end)) {
+                currPhase = phase.name;
+                break;
+            }
+        }
+
+        // Step 3.1 Fetch the existing phase file in S3
+        console.log('Fetching and parsing existing phase file from S3...');
+        let currPhaseData = [];
+        try {
+            const response = await s3.send(new GetObjectCommand({ Bucket: S3_BUCKET, Key: `data/${currPhase}.json` }));
             const bodyContents = await response.Body.transformToString();
-            masterData = JSON.parse(bodyContents);
+            currPhaseData = JSON.parse(bodyContents);
         } catch (e) {
             // Suppress no file found since we can just start with an empty array
             if (e.name !== 'NoSuchKey') {
                 console.error('Error fetching S3 data:', e);
                 return res.status(500).json({ message: 'Error accessing master data' });
+            } else {
+                console.log('No existing phase file found, creating new one...');
             }
         }
-        console.log('Successfully fetched and parsed existing master file from S3');
+        console.log('Successfully fetched and parsed existing phase file from S3');
 
         // Step 3.2 Validate response
         console.log('Validating response...');
@@ -45,8 +67,8 @@ app.post('/survey', async (req, res) => {
         };
 
         try {
-            const { version } = validateResponse(newResponse);
-            console.log(`Response validated successfully using format version ${version}`);
+            const surveyVersion = validateResponse(newResponse);
+            console.log(`Response validated successfully using survey version ${surveyVersion}`);
         } catch (e) {
             console.error('Validation failed:', e);
             console.log('Storing invalid response...');
@@ -55,15 +77,15 @@ app.post('/survey', async (req, res) => {
 
         // Step 3.3 Append the new response
         console.log('Appending new response...');
-        masterData.push(newEntry);
+        currPhaseData.push(newEntry);
 
         // Step 3.4 Write the updated data back to S3
         console.log('Writing updated data back to S3...');
         await s3.send(new PutObjectCommand({
             Bucket: S3_BUCKET,
-            Key: S3_KEY,
+            Key: `data/${currPhase}.json`,
             ContentType: 'application/json',
-            Body: JSON.stringify(masterData),
+            Body: JSON.stringify(currPhaseData),
         }));
 
         console.log('Successfully saved updated data to S3');
