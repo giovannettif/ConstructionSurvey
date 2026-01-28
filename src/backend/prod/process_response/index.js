913958221c36d276ee3fbdbe0d68f8b62307c38f
Krish A. Patel <krishpatel2067@gmail.com>
2026-01-28 16:22:20 -0500

    Clean up index.js

--- src/backend/prod/process_response/index.js
+++ src/backend/prod/process_response/index.js
@@ -4,97 +4,99 @@ import express from 'express';
 import serverless from 'serverless-http';
 import { S3Client, GetObjectCommand, PutObjectCommand } from '@aws-sdk/client-s3';
 import { validateResponse } from './validateResponse.js';
-// Step 1: S3 Configuration
+// S3 Configuration
 const S3_BUCKET = process.env.S3_BUCKET_NAME;
-const s3 = new S3Client({ region: process.env.AWS_REGION }); // AWS_REGION is pre-defined by AWS
+const s3 = new S3Client({ region: process.env.AWS_REGION });
 
-// Step 2: Express.js Setup
+// Express.js Setup
 const app = express();
 app.use(express.json());
 
-// Step 3: Handle new survey responses
+// handle new survey responses
 app.post('/survey', async (req, res) => {
+    let message;
+    const { data: newResponse } = req.body;
+
+    console.log('1. Reading phases file...');
+    let phasesData = [];
+
     try {
-        const { data: newResponse } = req.body;
+        const phases = await s3.send(new GetObjectCommand({ Bucket: S3_BUCKET, Key: 'metadata/phases.json' }));
+        const bodyContents = await phases.Body.transformToString();
+        phasesData = JSON.parse(bodyContents);
+    } catch (e) {
+        message = 'Error fetching phases file';
+        console.error(message, e);
+        return res.status(500).json({ message });
+    }
 
-        console.log('Reading phases file...');
-        let phasesData = [];
+    // find current phase
+    const currDate = new Date();
+    // default to current month and year
+    let currPhase = currDate.getMonth() + 1 + "-" + currDate.getFullYear();
 
-        try {
-            const phases = await s3.send(new GetObjectCommand({ Bucket: S3_BUCKET, Key: 'metadata/phases.json' }));
-            const phasesBodyContents = await phases.Body.transformToString();
-            phasesData = JSON.parse(phasesBodyContents);
-        } catch (e) {
-            console.error('Error fetching current phase:', e);
-            return res.status(500).json({ message: 'Error accessing phases file' });
+    for (const phase of phasesData) {
+        if (currDate >= new Date(phase.start) && currDate <= new Date(phase.end)) {
+            currPhase = phase.name;
+            break;
         }
+    }
 
-        const currDate = new Date();
-        let currPhase = currDate.getMonth() + 1 + "-" + currDate.getFullYear();
-        for (const phase of phasesData) {
-            if (currDate >= new Date(phase.start) && currDate <= new Date(phase.end)) {
-                currPhase = phase.name;
-                break;
-            }
+    console.log('2. Fetching current phase file from S3...');
+    let currPhaseData = [];
+    try {
+        const response = await s3.send(new GetObjectCommand({ Bucket: S3_BUCKET, Key: `data/${currPhase}.json` }));
+        const bodyContents = await response.Body.transformToString();
+        currPhaseData = JSON.parse(bodyContents);
+    } catch (e) {
+        // Suppress no file found since we can just start with an empty array
+        if (e.name !== 'NoSuchKey') {
+            message = 'Error fetching current phase file';
+            console.error(message, e);
+            return res.status(500).json({ message });
+        } else {
+            console.log('No existing phase file found, creating new one...');
         }
+    }
 
-        // Step 3.1 Fetch the existing phase file in S3
-        console.log('Fetching and parsing existing phase file from S3...');
-        let currPhaseData = [];
-        try {
-            const response = await s3.send(new GetObjectCommand({ Bucket: S3_BUCKET, Key: `data/${currPhase}.json` }));
-            const bodyContents = await response.Body.transformToString();
-            currPhaseData = JSON.parse(bodyContents);
-        } catch (e) {
-            // Suppress no file found since we can just start with an empty array
-            if (e.name !== 'NoSuchKey') {
-                console.error('Error fetching S3 data:', e);
-                return res.status(500).json({ message: 'Error accessing master data' });
-            } else {
-                console.log('No existing phase file found, creating new one...');
-            }
-        }
-        console.log('Successfully fetched and parsed existing phase file from S3');
+    console.log('3. Validating response...');
+    const newEntry = {
+        id: new Date().getTime(),
+        s3_timestamp: new Date().toISOString(),
+        uploaded_to_drive: false,
+        valid: true,
+        data: newResponse,
+    };
 
-        // Step 3.2 Validate response
-        console.log('Validating response...');
-        const newEntry = {
-            id: new Date().getTime(),
-            s3_timestamp: new Date().toISOString(),
-            uploaded_to_drive: false,
-            valid: true,
-            data: newResponse,
-        };
+    try {
+        const surveyVersion = validateResponse(newResponse);
+        console.log(`Response validated successfully using survey version ${surveyVersion}`);
+    } catch (e) {
+        message = 'Validation failed';
+        console.error(message, e);
+        console.log('Storing invalid response...');
+        newEntry.valid = false;
+    }
 
-        try {
-            const surveyVersion = validateResponse(newResponse);
-            console.log(`Response validated successfully using survey version ${surveyVersion}`);
-        } catch (e) {
-            console.error('Validation failed:', e);
-            console.log('Storing invalid response...');
-            newEntry.valid = false;
-        }
+    currPhaseData.push(newEntry);
 
-        // Step 3.3 Append the new response
-        console.log('Appending new response...');
-        currPhaseData.push(newEntry);
-
-        // Step 3.4 Write the updated data back to S3
-        console.log('Writing updated data back to S3...');
+    console.log('4. Writing updated data back to S3...');
+    try {
         await s3.send(new PutObjectCommand({
             Bucket: S3_BUCKET,
             Key: `data/${currPhase}.json`,
             ContentType: 'application/json',
             Body: JSON.stringify(currPhaseData),
         }));
-
-        console.log('Successfully saved updated data to S3');
-        return res.status(200).json({ message: 'Survey response saved successfully!' });
     } catch (e) {
-        console.error('Error saving to S3:', e);
-        return res.status(500).json({ message: 'Error saving survey response' });
+        message = 'Error writing updated data back to S3';
+        console.error(message, e);
+        return res.status(500).json({ message });
     }
+
+    message = 'Survey response saved successfully!';
+    console.log(message);
+    return res.status(200).json({ message });
 });
 
-// The handler function required by AWS Lambda
 export const handler = serverless(app);
