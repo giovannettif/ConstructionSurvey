de8a65e3c3a5b5fb3e8887c23ce81f7471f3cb33
Franco <franco@project-osi.local>
2026-05-02 22:18:53 -0400

    fix: restore site_id empty string default

--- src/frontend_vanilla/static/script.js
+++ src/frontend_vanilla/static/script.js
@@ -1160,7 +1160,7 @@ class DynamicSurvey {
         surveyTitle: this.config.title,
         surveyVersion: this.config.version,
         mode: this.getStoredMode(),
-        site_id: query.site_id || query.site || null,
+        site_id: query.site_id || query.site || "",
         query,
         gps,
         answers: allAnswers,
